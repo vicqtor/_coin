451a30b86fb535469168646ec9c994584ec45e4a
mwangi victor <tmvicbvn@gmail.com>
2020-09-08 11:10:49 +0300

    Update cur_rates.py

--- cur_rates.py
+++ cur_rates.py
@@ -1,30 +1,24 @@
-from bs4 import BeautifulSoup
+import json
 import requests
 import notify2
+from bs4 import BeautifulSoup
 
-def f():
-    try:
-        url = 'https://www.coingecko.com/en/price_charts/bitcoin/inr'
-        # header = {'user-Agent': 'Mozilla/5.0'}
-        bitfile=requests.get(url)
-   
-        sp=BeautifulSoup(bitfile.text,'html.parser')
-     
-        bits=[]
-        for tbl in sp.find_all('table',attrs={'class':'table'}):
-            for td in tbl.find_all('td'):
-                bits.append(td.text)
-       
-        del bits[3:]
-        bits = list(map(lambda s : s.strip(), bits))
-        
-        # return bits
-    except:bits = []
+def getrates():
+    url = 'https://www.coingecko.com/en/price_charts/bitcoin/inr' # header = {'user-Agent': 'Mozilla/5.0'}
+    
+    bitfile = requests.get(url)
+    soup_object = BeautifulSoup(bitfile.text,'html.parser')
+
+    bits = []
+    for tbl in soup_object.find_all('table', attrs = {'class':'table'}):
+        for td in tbl.find_all('td'):
+            bits.append(td.text)
+
+    del bits[3:]
+    bits = list(map(lambda s : s.strip(), bits))
+
     return bits
 
-rts=f()
-
-res =''
-
-if len(rts) != 0:
-    res=res+str(rts[0])+'-'+str(rts[2].encode('utf-8'))+'\n'
+rates = getrates()
+rates_set = ''
+rates_set = rates_set + rates[0] + ':' + rates[2].encode('utf-8') + '\n'
