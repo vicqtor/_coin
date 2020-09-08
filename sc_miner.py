ab1c5e09e0c0615aa4836035a39019fa07b1252d
mwangi victor <tmvicbvn@gmail.com>
2020-09-08 11:29:16 +0300

    Update sc_miner.py

--- sc_miner.py
+++ sc_miner.py
@@ -1,14 +1,14 @@
+from scoin import BlockChain
 
-try:
-    from scoin import BlockChain
-    bc = BlockChain()
-    print('\n[scoin]\n\nmining...\n' + str(bc.chain))
-    lastblock = bc.latestblock
-    lastproofno = lastblock.proofno
-    proofno = bc.proofofwork(lastproofno)
-    # 0 implies that this node has created a new block; 1 creating a new block (or identifying the proof number) is awarded with 1
-    bc.newdata(sender = '0', recipient = 'Quincy Larson', quantity = 1)
-    lasthash = lastblock.calculatehash
-    block = bc.constructblock(proofno, lasthash)
-    print('scoin mined\n' + str(bc.chain) + '\n')
-except:pass
+bc = BlockChain()
+print(bc.chain + ' mining')
+
+lastblock = bc.latestblock
+lastproofno = lastblock.proofno
+proofno = bc.proofofwork(lastproofno)
+
+bc.newdata(sender = '0', recipient = 'mwangi victor', quantity = 1)
+lasthash = lastblock.calculatehash
+block = bc.constructblock(proofno, lasthash)
+
+print('mined\n' + bc.chain)
