8a102cbdb610b55f8effe0e97ac908d8e80e1927
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 12:03:19 +0300

    Update sc_miner.py

--- sc_miner.py
+++ sc_miner.py
@@ -1,7 +1,20 @@
+import notify2
 from scoin import BlockChain
 
+'''
+infinite loop:
+ - cur_rates
+'''
+
+def alert(info):
+  notify2.init('sc pfm')
+  notif = notify2.Notification('scoin', info)
+	# notif.set_urgency(notify2.URGENCY_CRITICAL)
+  notif.show()
+  notif.set_timeout(10)
+
 bc = BlockChain()
-print(bc.chain + ' mining')
+alert(bc.chain + ' mining')
 
 lastblock = bc.latestblock
 lastproofno = lastblock.proofno
@@ -11,4 +24,6 @@ bc.newdata(sender = '0', recipient = 'mwangi victor', quantity = 1)
 lasthash = lastblock.calculatehash
 block = bc.constructblock(proofno, lasthash)
 
-print('mined\n' + bc.chain)
+alert('mined\n' + bc.chain)
+
+# /trns/trns_mgr.
